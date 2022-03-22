26acec196e8a9bba6ea36e692e8ed9ad2c00cffa
LienPaul <zinfu1111@gmail.com>
2022-03-23 00:35:21 +0800

    Initial Commit

---
+++ amazingtalker_paul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amazingtalker_paul
+//
+//  Created by 連振甫 on 2022/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
